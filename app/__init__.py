77df978c5422a8176c405c3205107b6a323e3a30
mimi tangitcharoen <mtangitcharoen@waimea.school.nz>
2025-05-13 09:59:49 +1200

    rrtr

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,11 @@ app = Flask(__name__)
 def home():
     return render_template("pages/home.jinja")
 
+@app.get("/test/")
+def test():
+    return render_template("pages/test.jinja")
 
+@app.get("/about/")
+def about():
+    return render_template("pages/about.jinja")
 
